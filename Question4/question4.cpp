573d792cf5c248e771604368375ae848dc5a0e97
vitoriapinheiro <vps3@cin.ufpe.br>
2021-11-30 10:54:36 -0300

    Co-authored-by: Davi Yan <daviyan5@users.noreply.github.com> Co-authored-by: TiagoHRPG <TiagoHRPG@users.noreply.github.com> Co-authored-by: Edson Neto <emn2@users.noreply.github.com>

--- Question4/question4.cpp
+++ Question4/question4.cpp
@@ -1,211 +1,79 @@
-//contrabando
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include "bits/stdc++.h"
 
-/*
-  Nesta questão serão necessários dois buffers e, consequentemente, dois Mutexes.
-  Serão necessários dois buffers pois, em um precisamos ter uma fila de execuções que ainda serão realizadas e
-  em outro precisamos ter uma lista de todos os resultados das execuções que já foram concluídas.
+#define endl '\n'
 
-  Tendo estes dois buffers, precisamos agora de dois Mutexes, um para cada buffer,
-  a fim de evitar que duas ou mais threads tenham acesso a uma região crítica (os buffers).
-*/
+using namespace std;
 
-#define N 5 // Número de processadores/núcleos do sistema, portanto, também representa a quantidade de threads do programa
-#define BUFFER_SIZE 10 // Tamanho máximo do Buffer
+int n, m;                                                       // linha e coluna da matriz
 
-typedef struct param { // Struct pedida pela questão que irá servir como parâmetro de uma função em uma execução
-  int a, b, c;
-  int execucaoId; // ID da execução a qual esse parâmetro está vinculado (será utilizado para colocar o resultado das funções no buffer de resultados)
-  int threadId;
-} Param;
-
-typedef struct execucao { // Para cada execução agendada na função agendarExecucao, será criada uma instância desta Struct
-  void *genericFunc;
-  Param funcParams;
-} Execucao;
-
-// Inicialização dos Mutexes e suas variáveis de condição.
-pthread_mutex_t mutexBufferExecucao = PTHREAD_MUTEX_INITIALIZER; // Mutex relacionado ao buffer de execuções
-pthread_mutex_t mutexBufferResult = PTHREAD_MUTEX_INITIALIZER; // Mutex relacionado ao buffer de resultados
-pthread_cond_t condExecucao = PTHREAD_COND_INITIALIZER; // Variavél de condição para o buffer de execução
-pthread_cond_t condResult = PTHREAD_COND_INITIALIZER; // Variavél de condição para o buffer de resultados
-
-// Variáveis globais que serão utilizadas no decorrer da questão
-Execucao *bufferExecucao;
-int statusBufferExecucoes = 0; // Tamanho atual do buffer de execucoes
-int *bufferResult;
-int statusBufferResult = 0; // Tamanho atual do buffer de resultados
-
-int qtdExecucoes = 0; // Irá funcionar como o ID das execuções, além disso, usaremos essa variável para comparar com o tamanho máximo do buffer e conferir se chegamos no limite
-int execucaoDaVez = 0;
-
-pthread_t threads[N];
-int statusThreads[N];
-
-int *idResultados;
-
-// Utilizaremos essas cores para printar as respostas das execuções
-char *cores[3] = {
-  "\033[48;2;255;0;0", // Vermelho
-  "\033[48;2;255;255;0", // Amarelo
-  "\033[48;2;0;255;0", // Verde
-};
-char *preto = ";30m";
-char *branco = "m";
-char *frases[3] = {
-  " ",
-  "funexecMod10 acabou de lançar um novo resultado no nosso sistema!\n",
-  "O resultado de ",
+struct Pixel{                                                   // Estrutura de argumentos, neste caso as cores de cada pixel
+    int r, g, b;
+    Pixel() : r(-1), g(-1), b(-1) {}
+    Pixel(int i, int j, int k) : r(i), g(j), b(k) {}
 };
 
+void *conversor(void *args){                                    // Função de cada thread, converte seu respectivo pixel para escala de cinza
+    int r = ((Pixel*)args)->r;
+    int g = ((Pixel*)args)->g;
+    int b = ((Pixel*)args)->b;
 
-// Início das funções
-int agendarExecucao(void *funexec, Param funcParams) {  // Primeira função pedida pela questão, aqui colocamos uma nova execução (tarefa) no buffer de execuções e retornamos o ID dessa execução
-  Execucao request;
-  request.funcParams = funcParams;
-  request.genericFunc = funexec;
+    int c = r*0.3 + g*0.59 + b*0.11;
 
-  pthread_mutex_lock(&mutexBufferExecucao); // Travando o mutex para que outras threads não tenham acesso ao recurso compartilhado (buffer e variáveis globais) enquanto mexemos nele
-
-  // printf("%s%d...\033[m\n", cores[0], qtdExecucoes);
-  printf("%s%s Por favor, aguarde um instante enquanto agendamos a execução %d no buffer.\033[m\n", cores[0], branco, qtdExecucoes);
-  request.funcParams.execucaoId = qtdExecucoes;
-  bufferExecucao[qtdExecucoes] = request;
-
-  if(qtdExecucoes == BUFFER_SIZE) qtdExecucoes = 0; // O buffer funciona de forma parecida a um array circular, portanto, caso cheguemos ao seu final, devemos retornar ao início
-  if(statusBufferExecucoes == 1) pthread_cond_broadcast(&condExecucao); // Caso essa seja a primeira execução dentro do buffer, devemos acordar as threads consumidoras
-  qtdExecucoes++;
-  statusBufferExecucoes++;
-  if(qtdExecucoes == BUFFER_SIZE) qtdExecucoes = 0;
-  if(statusBufferExecucoes == 1) pthread_cond_broadcast(&condExecucao); // Aqui conferimos novamente para atestar que as condições não foram atingidas após o incremento das variáveis
-
-  pthread_mutex_unlock(&mutexBufferExecucao);
-  return request.funcParams.execucaoId;
+    Pixel *ret = new Pixel(c,c,c);
+    pthread_exit((void *) ret);
 }
 
-int pegarResultadoExecucao(int execucaoId) { // Segunda função pedida pela questão, aqui checamos se há algum resultado de execuções no buffer de resultados, se sim, retornamos esse resultado, se não, adormecemos as threads até termos um resultado
-  int result;
-
-  pthread_mutex_lock(&mutexBufferResult); // Travando o mutex para que outras threads não tenham acesso ao recurso compartilhado (buffer e variáveis globais) enquanto mexemos nele
-
-  while(statusBufferResult == 0 || bufferResult[execucaoId] < 0) { // Enquanto o buffer estiver vazio ou então o resultado dessa execução em específico não estiver disponível, esperamos
-    pthread_cond_wait(&condResult, &mutexBufferResult);
-  }
-
-  printf("%s%s Por favor, aguarde um instante enquanto coletamos o resultado da execução %d.\033[m\n", cores[1], preto, execucaoId);
-  result = bufferResult[execucaoId];
-  bufferResult[execucaoId] = 0;
-  statusBufferResult--;
-
-  pthread_mutex_unlock(&mutexBufferResult);
-  return result;
-}
-
-void *funexecAddMod10(void* params) {
-  Param *aux = (Param *) params;
-  int result = (aux->a + aux->b + aux->c) % 10;
-
-  pthread_mutex_lock(&mutexBufferResult);
-
-  bufferResult[aux->execucaoId] = result;
-  statusBufferResult++;
-
-  pthread_cond_signal(&condResult); // Avisa às threads que um resultado foi adicionado ao buffer de resultados
-  pthread_mutex_unlock(&mutexBufferResult);
-
-  statusThreads[aux->threadId] = -1;
-  pthread_exit(NULL);
-}
-
-void *funcThreadDespachante() { // Esta função será chamada quando a thread despachante for criada
-  int i;
-  pthread_mutex_lock(&mutexBufferExecucao);
-
-  while(statusBufferExecucoes == 0) { // Aqui sinalizamos às threads do buffer de execução que ainda não existe nenhuma execução pendente, fazemos algo parecido na sexta questão, com o put da fila bloqueante
-    pthread_cond_wait(&condExecucao, &mutexBufferExecucao);
-  }
-
-  while(statusBufferExecucoes > 0) { // Aqui rodamos um laço de repetição que só irá terminar quando não houver mais nenhuma execução pendente no buffer
-    for(i = 0; i < N && bufferExecucao[execucaoDaVez].genericFunc != NULL; i++) {
-      if(statusThreads[i] == -1) {
-        statusThreads[i] = 1;
-        bufferExecucao[execucaoDaVez].funcParams.threadId = i;
-        pthread_create(&threads[i], NULL, bufferExecucao[execucaoDaVez].genericFunc, (void *) &bufferExecucao[execucaoDaVez].funcParams);
-      }
-      bufferExecucao[execucaoDaVez].genericFunc = NULL;
-
-      printf("A thread despachante acabou de despachar a execucao: %d\n", execucaoDaVez);
-      if (execucaoDaVez == BUFFER_SIZE) execucaoDaVez = 0; // Assim como em um array circular, aqui voltamos a checar o início do buffer de execuções
-      if (statusBufferExecucoes == 0) break;
-      statusBufferExecucoes--;
-      execucaoDaVez++;
-      if (execucaoDaVez == BUFFER_SIZE) execucaoDaVez = 0; // Aqui checamos novamente para garantir que as condições não foram atingidas desde o último incremento
-      if (statusBufferExecucoes == 0) break;
+int main(){
+    ifstream imagem;
+    imagem.open("image.ppm");                                   // abre o arquivo de entrada e lê os valores
+    if(!imagem){
+        cout << "Ocorreu um erro ao abrir a imagem" << endl;
+    }
+    vector<vector<Pixel> > matriz_pixel,matriz_pixel_saida;     // matrizes de entrada e saída
+    string tipo;
+    int valor_max;
+    imagem >> tipo >> m >> n >> valor_max;                      // Leitura dos atributos da imagem
+    
+    matriz_pixel.resize(n);
+    matriz_pixel_saida.resize(n);
+    
+    for(int i = 0; i < n; i++){
+        matriz_pixel[i].resize(m);
+        for(int j = 0; j < m; j++){
+            int r, g, b;
+            imagem >> r >> g >> b;
+            matriz_pixel[i][j] = Pixel(r, g, b);                // Leitura dos pixels
+        }
     }
 
-    if (statusBufferExecucoes == 0) pthread_cond_wait(&condExecucao, &mutexBufferExecucao);
-  }
-
-  pthread_mutex_unlock(&mutexBufferExecucao);
-  pthread_exit(NULL);
-}
-
-int main(int argc, char *argv[]) {
-  int i, result;
-  Param aux;
-
-  bufferExecucao = (Execucao *) malloc(BUFFER_SIZE * sizeof(Execucao));
-  bufferResult = (int *) malloc(BUFFER_SIZE * sizeof(int));
-  idResultados = (int *) malloc(BUFFER_SIZE * sizeof(int));
-
-  for(i = 0; i < BUFFER_SIZE; i++) bufferResult[i] = -1; // Iniciando todos os resultados do buffer de resultados como -1 para que saibamos que ainda não temos um resultado válido nesta posição
-  for(i = 0; i < N; i++) statusThreads[i] = -1; // Iniciando todas as threads com o status -1 para indicar que elas não estão ocupadas, portanto pode receber outra funexec
-
-  pthread_t despachante; // Thread pedida pela questão, ela irá pegar as requisições do buffer e gerenciar as N threads para saber qual vai ficar responsável por cada requisição
-  pthread_create(&despachante, NULL, funcThreadDespachante, NULL);
-
-  for(i = 0; i < BUFFER_SIZE; i++) {
-    // Essa série de IF's abaixo são apenas para diversificar mais os resultados encontrados
-    if (i == 1) {
-      aux.a = 1;
-      aux.b = 2;
-      aux.c = 3;
-    } else if (i == 2) {
-      aux.a = 1;
-      aux.b = 2;
-      aux.c = 3;
-    } else if (i % 3 == 0) {
-      aux.a = 3;
-      aux.b = 6;
-      aux.c = 9;
-    } else if (i == 9) {
-      aux.a = 9;
-      aux.b = 8;
-      aux.c = 7;
-    } else {
-      aux.a = 94;
-      aux.b = 88;
-      aux.c = 75;
+    pthread_t threads_pixel[n][m];
+    
+    for(int i = 0; i < n; i++){                                 // Cria as threads
+        for(int j = 0; j < m; j++){
+            pthread_create(&threads_pixel[i][j], NULL, conversor, &matriz_pixel[i][j]); 
+        }
+    }
+    
+    for(int i = 0; i < n; i++){                                 // Espera as threads e o valor de retorno
+        matriz_pixel_saida[i].resize(m);
+        for(int j = 0; j < m; j++){
+            Pixel *ret;
+            pthread_join(threads_pixel[i][j],(void **) &ret);
+            matriz_pixel_saida[i][j] = *(ret);
+        }
+    }
+    
+    ofstream imagem_saida;
+    imagem_saida.open("imagem_saida.ppm");                      // Gera o arquivo de saida
+    imagem_saida << tipo << endl;
+    imagem_saida << m << " " << n << endl;
+    imagem_saida << valor_max << endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            imagem_saida << matriz_pixel_saida[i][j].r << " " <<  matriz_pixel_saida[i][j].g << " " << matriz_pixel_saida[i][j].b << endl;
+        }
     }
 
-    idResultados[i] = agendarExecucao((void *) funexecAddMod10, aux);
-  }
-
-  for(int i = 0; i < BUFFER_SIZE; i++) {
-    result = pegarResultadoExecucao(idResultados[i]);
-    printf("%s%s Veja, conseguimos o resultado da execução %d: %d.\033[m\n", cores[2], preto, idResultados[i], result);
-  }
-
-  for(i = 0; i < N; i++) pthread_join(threads[i], NULL);
-
-  pthread_join(despachante, NULL);
-  printf("Fim do programa.\n");
-
-  free(bufferExecucao);
-  free(bufferResult);
-  pthread_exit(NULL);
-  return 0;
+    return 0;
 }
--- Question7/question7.cpp
+++ Question7/question7.cpp
@@ -1,130 +1,198 @@
-//contrabando
-/*
-    Nessa questão o uso  concorrente de  memória não foi uma  preocupação por conta de cada  pixel da
-    imagem  trabalhar de  forma  isolada, ou  seja, foi possível  isolar  a  conversão  dos pixels em
-    diferentes Threads. Desse modo, a implementação se deu de forma que a leitura e escrita dos dados
-    é feita em uma única oportunidade e cada Thread fica responsável apenas por converter um conjunto
-    específico de pixels. 
+#include <pthread.h>
+#include "bits/stdc++.h"
 
-    Sendo  assim,  após  feita a leitura  dos dados do arquivo de imagem, uma  estrutura de array de 2
-    dimensões é  criada  com  esses dados. Essa estrutura  foi construida  para ser um array de struct
-    com os dados  RGB  de  cada pixel. Em contrapartida, há  um  array  bi-dimensional  que  guarda os
-    dados  após  a  conversão. Para  fins  de  facilitar  o  desenvolvimento, essas  estruturas  forma
-    declaradas de forma global.
-*/
+using namespace std;
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h> 
+#define endl '\n'
+#define NUM_THREADS 10
+#define MOD 1000000007
 
-// Quantidade de Threads a ser usada na execução do código 
-#define THREADS 3
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+pthread_mutex_t mutex_buffer_funcoes = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t vazio = PTHREAD_COND_INITIALIZER;
 
-// Struct com os elemtnos RGB
-typedef struct Elem{
-    int R, G, B;
-}Elem;
+struct Param{
+    int id, a, b;
+    int thread_id;
+    Param() : id(-1), a(-1), b(-1) {}
+    Param(int i, int x, int y) : id(i), a(x), b(y) {}
+};
 
-// Array que guarda os valores convertidos
-int **mat;
-// Array que guarda os valores lidos do arquivo
-Elem **pixel;
-// Variáveis de uso em laços (i, j) | Quantidade de Linhas de Colunas (LIN, COL)
-int i, j, LIN, COL;
+struct Exec{
+    int tipo;
+    Param args;
+    Exec() : tipo(-1), args() {}
+    Exec(int t, Param p) : tipo(t), args(p) {}
+};
 
-// Função que será executada dentro de cada Thread, seu obejtivo consiste em converter os
-// dados  RBG em Tons de cinza 
-void *conversao(void * info) {
-    // Recebe o id da thread, esse dado consiste em um número que identifica qual a thread
-    // que executará naquele instante.
-    int* id = (int*) info;
-    // Esse laço tem por objetivo varrer o array bi-dimensional de uma forma que a Thread
-    // atual leia somente os pixels que outras threads não lerão
-    for(int i = *id; i < (LIN * COL); i+=THREADS){
-        int k = i/COL, j = i%COL;
-        // Converte usando os dados fornecidos na questão
-        mat[k][j] = (int) (pixel[k][j].R*0.30 + pixel[k][j].G*0.59 + pixel[k][j].B*0.11);
+ofstream arq_saida;
+bool terminou = false;
+vector<bool> sinal_thread;
+queue<Exec> buffer_funcoes;
+unordered_map<int, bool> criou_id;
+unordered_map<int, int> buffer_resultados;
+
+int gera_id(){
+    int id = rng() % MOD;
+    while (criou_id[id]){
+        id = rng() % MOD;
     }
+    criou_id[id] = true;
+    return id;
+}
+
+void *soma_int(void *args){
+    cout << "......................................." << endl;
+    int a = ((Param *)args)->a;
+    int b = ((Param *)args)->b;
+    int id = ((Param *)args)->id;
+    int thread_id = ((Param *)args)->thread_id;
+
+
+    int res = (a + b) % MOD;
+    buffer_resultados[id] = res;
+    sinal_thread[thread_id] = false;
     pthread_exit(NULL);
 }
 
-int main () {
-    pthread_t thread[THREADS];
-    FILE *img;
-    char texto[20];
-    int status, *threadID, max;
+void *diff_quadrados(void *args){
+    cout << ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,," << endl;
+    int a = ((Param *)args)->a;
+    int b = ((Param *)args)->b;
+    int id = ((Param *)args)->id;
+    int thread_id = ((Param *)args)->thread_id;
+    
+    int res = (int)((long long)(a + b) * (a - b) % MOD);
+    buffer_resultados[id] = res;
+    sinal_thread[thread_id] = false;
+    pthread_exit(NULL);
+}
 
-    // abrindo a imagem
-    img = fopen("img.ppm", "r");
-    if(img == NULL) {
-        printf("Erro na abertura do arquivo!");
+void *mult_int(void *args){
+    cout << "----------------------------------------" << endl;
+    int a = ((Param *)args)->a;
+    int b = ((Param *)args)->b;
+    int id = ((Param *)args)->id;
+    int thread_id = ((Param *)args)->thread_id;
+
+    int res = (int)((long long)(a * b) % MOD);
+    buffer_resultados[id] = res;
+    sinal_thread[thread_id] = false;
+
+    pthread_exit(NULL);
+}
+
+void *despachante(void * args){
+    pthread_t thread_exec[NUM_THREADS];
+    
+    while(true){
+        cout << "Entrou no while" << endl;
+        while(buffer_funcoes.size() == 0){
+            if(terminou) pthread_exit(NULL);
+            pthread_cond_wait(&vazio, &mutex_buffer_funcoes);
+        }
+        cout << "Saiu do while\n";
+
+        Exec atual = buffer_funcoes.front();
+        buffer_funcoes.pop();
+        
+        int i = 0;
+        bool executou = false;
+        while(!executou){
+            cout << "Continua no while" << endl;
+            if(!sinal_thread[i]){
+                executou = true;
+                sinal_thread[i] = true;
+                atual.args.thread_id = i;
+                switch(atual.tipo){
+                    case 0:
+                        pthread_create(&thread_exec[i], NULL, soma_int, &(atual.args));
+                        break;
+                    case 1:
+                        pthread_create(&thread_exec[i], NULL, diff_quadrados, &(atual.args));
+                        break;
+                    case 2:
+                        pthread_create(&thread_exec[i], NULL, mult_int, &(atual.args));
+                        break;
+                    default:
+                        cout << "Ocorreu um erro ao selecionar a função" << endl;
+                }
+            }
+            i = (i + 1) % NUM_THREADS;
+        }
+        cout << "Passou do segundo While\n";
+    }
+
+    pthread_exit(NULL);
+}
+
+int agendar_execucao(int tipo, Param args){
+    Exec buffer_params(tipo, args);
+
+    pthread_mutex_lock(&mutex_buffer_funcoes);
+    
+    buffer_funcoes.push(buffer_params);
+    pthread_cond_broadcast(&vazio);
+
+    cout << "Função de ID: " << args.id << " agendada!" << endl;
+
+    pthread_mutex_unlock(&mutex_buffer_funcoes);
+    return args.id;
+}
+
+int pegar_resultado_execucao(int id){
+    while(!buffer_resultados.count(id)) continue;
+    return buffer_resultados[id];
+}
+
+int main(){
+    sinal_thread.assign(NUM_THREADS, false);
+
+    pthread_t thread_despachante;
+    pthread_create(&thread_despachante, NULL, despachante, NULL);
+
+    arq_saida.open("saida.txt");
+    if(!arq_saida){
+        cout << "Ocorreu um erro ao ler o arquivo de saída";
         return 1;
     }
 
-    // Leitura das dimensões e valor maximo
-    fgets(texto, 20, img);
-    fscanf(img, "%d %d", &COL, &LIN);
-    fscanf(img, "%d", &max);
+    vector<int> id_exec;
+    int num_func = (rng() % (MOD / 1000000)) + 100;
+    cout << "Criando arquivo de saída..." << endl;
 
-    // Alocaçao do array
-    mat = (int **) malloc(LIN * sizeof(int *));
-    for (i=0; i < LIN; i++)
-        mat[i] = (int *) malloc(COL * sizeof(int));
+    for(int i = 0; i < num_func; i++){
+        int id = gera_id();
+        int tipo = (rng() % 3);
+        int a = (rng() % MOD), b = (rng() % MOD);
 
+        Param p(id, a, b);
+
+        arq_saida << "Gerou função com ID " << id << " do tipo " << tipo << " de parametros " << a << " e " << b << endl;
+
+        switch (tipo){
+        case 0:
+            agendar_execucao(tipo, p);
+            break;
+        case 1:
+            agendar_execucao(tipo, p);
+            break;
+        case 2:
+            agendar_execucao(tipo, p);
+            break;
+        default:
+            cout << "Essa função não foi prevista pelo sistema" << endl;
+        }
+        id_exec.push_back(id);
+    }
     
-    // Alocaçao do array de id
-    threadID = (int *) malloc(THREADS * sizeof(int));
-
-    pixel = (Elem **) malloc(LIN * sizeof(Elem));
-    for (i=0; i < LIN; i++)
-        pixel[i] = (Elem*) malloc(COL * sizeof(Elem));
-    
-    // Leitura dos pixels da imagem
-    for(i = 0; i < LIN; i++) {
-        for(j = 0; j < COL; j++){
-            fscanf(img, "%d %d %d", &pixel[i][j].R, &pixel[i][j].G, &pixel[i][j].B);
-        }
+    terminou = true;
+    //Checa todas os id executados, se já houve execução
+    for (auto &id : id_exec){
+        arq_saida << "Resultado para o id: " << id << " foi " << pegar_resultado_execucao(id) << endl;
     }
 
-    // Criação das threads
-    for (i = 0; i < THREADS; i++){
-        threadID[i] = i; 
-        status = pthread_create(&thread[i],NULL,conversao,(void *) &threadID[i]); 
-        if(status){
-            printf("Erro ao criar Thread!");
-            exit(-1);
-        }
-    }
+    cout << "O arquivo de saída já está pronto!" << endl;
 
-    // Aguarda a execução das Threads
-    for(i = 0; i < THREADS; i++) {
-        pthread_join(thread[i],NULL);
-    }
-
-    // Gerando nova imagem
-    img = fopen("new-image.ppm", "w");
-    if(img == NULL) {
-        printf("Erro na criação do arquivo!");
-        return 1;
-    }
-
-    // Escrita dos dados convertidos
-    fprintf(img, "P3\n%d %d\n%d\n", COL, LIN, max);
-    for(i = 0; i < LIN; i++) {
-        for(j = 0; j < COL; j++){
-            int aux = mat[i][j];
-            fprintf(img, "%d %d %d\n", aux, aux, aux); 
-        }
-    }
-
-    // Desalocando os arrays    
-    for(i = 0; i < LIN; i++){
-        free(mat[i]);
-        free(pixel[i]);
-    }
-    free(mat);
-    free(pixel);
-
-    fclose(img);
     return 0;
 }
